218ac7bfdaa70082958bea9c235460633c3c8e0e
villegasmaster <jmpolidura@gmail.com>
2017-12-10 14:05:32 +0100

    Finish project

--- DetalleFormulario.java
+++ DetalleFormulario.java
@@ -1,13 +1,80 @@
 package com.villegas.formulario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class DetalleFormulario extends AppCompatActivity {
+
+    private TextView tvNombre;
+    private TextView tvTelefono;
+    private TextView tvEmail;
+    private TextView tvDate;
+    private TextView tvDesc;
+    private Button btnEditar;
+
+    String nombre;
+    String fecha;
+    String telefono;
+    String email;
+    String descripcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_formulario);
+        Bundle params = getIntent().getExtras();
+
+        nombre       = params.getString(getResources().getString(R.string.username));
+        fecha        = params.getString(getResources().getString(R.string.date));
+        telefono     = params.getString(getResources().getString(R.string.telephone));
+        email        = params.getString(getResources().getString(R.string.email));
+        descripcion  = params.getString(getResources().getString(R.string.descripcion));
+
+
+        tvNombre = (TextView) findViewById(R.id.tvNombre);
+        tvDate = (TextView) findViewById(R.id.etDate);
+        tvTelefono = (TextView) findViewById(R.id.tvTelefono);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvDesc = (TextView) findViewById(R.id.tvDesc);
+        btnEditar = (Button) findViewById(R.id.btnEditar);
+
+        tvNombre.setText(nombre);
+        tvDate.setText(fecha);
+        tvTelefono.setText(telefono);
+        tvEmail.setText(email);
+        tvDesc.setText(descripcion);
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetalleFormulario.this, MainActivity.class);
+                intent.putExtra(getResources().getString(R.string.username) ,nombre);
+                intent.putExtra(getResources().getString(R.string.date), fecha);
+                intent.putExtra(getResources().getString(R.string.telephone), telefono);
+                intent.putExtra(getResources().getString(R.string.email), email);
+                intent.putExtra(getResources().getString(R.string.descripcion), descripcion);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            Intent intent = new Intent(DetalleFormulario.this, MainActivity.class);
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,12 @@
 package com.villegas.formulario;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -13,13 +16,36 @@ public class MainActivity extends AppCompatActivity {
 
     EditText date;
     DatePickerDialog datePickerDialog;
+    Button siguiente;
+    EditText nombre;
+    EditText telefono;
+    EditText email;
+    EditText descripcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        date = (EditText) findViewById(R.id.date);
+        date        = (EditText) findViewById(R.id.etDate);
+        nombre      = (EditText) findViewById(R.id.etUsername);
+        telefono    = (EditText) findViewById(R.id.etTelephone);
+        email       = (EditText) findViewById(R.id.etEmail);
+        descripcion = (EditText) findViewById(R.id.etDescription);
+        siguiente   = (Button) findViewById(R.id.btnSiguiente);
+
+
+
+        if(getIntent().getExtras() != null && !getIntent().getExtras().isEmpty()) {
+
+            nombre.setText(getIntent().getExtras().getString(getResources().getString(R.string.username)));
+            date.setText(getIntent().getExtras().getString(getResources().getString(R.string.date)));
+            telefono.setText(getIntent().getExtras().getString(getResources().getString(R.string.telephone)));
+            email.setText(getIntent().getExtras().getString(getResources().getString(R.string.email)));
+            descripcion.setText(getIntent().getExtras().getString(getResources().getString(R.string.descripcion)));
+
+        }
+
 
         date.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +71,21 @@ public class MainActivity extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, DetalleFormulario.class);
+                intent.putExtra(getResources().getString(R.string.username) , nombre.getText().toString());
+                intent.putExtra(getResources().getString(R.string.date), date.getText().toString());
+                intent.putExtra(getResources().getString(R.string.telephone), telefono.getText().toString());
+                intent.putExtra(getResources().getString(R.string.email), email.getText().toString());
+                intent.putExtra(getResources().getString(R.string.descripcion), descripcion.getText().toString());
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
